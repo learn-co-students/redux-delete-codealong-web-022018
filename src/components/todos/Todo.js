5c5d4989a25cea8480da952c6bffc2a164079eb1
lidianvilazio <lidianvilazio@hotmail.com>
2018-05-11 11:41:50 -0400

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
+
+  handleOnClick() {
+    this.props.store.dispatch({
+      type: 'DELETE_TODO',
+      id: this.props.id,
+    });
+}
+
   render() {
+    console.log("props",this.props);
+    // console.log(this.props.id);
     return (
       <li>
         {this.props.text}
+        <button onClick={(e) => {this.handleOnClick()}} />
       </li>
     );
   }
